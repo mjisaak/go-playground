af5af5f96522e1566a20e2bf7224f4c92144bdd9
Martin Brandl <martin.brandl@whiteduck.de>
2020-03-24 08:28:24 +0100

    struct

--- src/hello.go
+++ src/hello.go
@@ -6,10 +6,23 @@ import (
 	"fmt"
 )
 
-func main() {
-	fmt.Println("Counting:")
+type Player struct {
+	Name string
+	Age  int
+}
 
-	for i := 0; i < 10; i++ {
-		defer fmt.Println("", i)
+func main() {
+
+	vi := []struct {
+		Name string
+		Age  int
+	}{
+		{Name: "Martin", Age: 34},
+		{Name: "Chris", Age: 33},
 	}
+
+	fmt.Println("Hello everybody. This is Player:", vi[0].Name)
+
+	fmt.Println("And he is together with", vi[1].Name)
+
 }
