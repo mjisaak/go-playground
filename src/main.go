82010828c0c1d380b91f903e57adadc2d0fd82cb
Martin Brandl <martin.brandl@whiteduck.de>
2020-03-28 13:53:48 +0100

    added go

--- src/main.go
+++ src/main.go
@@ -4,24 +4,18 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
+
+type Player struct {
+	fartingPower int
+	name         string
+}
 
 func main() {
 
-	board := [][]string{
-		[]string{"_", "_", "_"},
-		[]string{"_", "_", "_"},
-		[]string{"_", "_", "_"},
+	players := []Player{
+		Player{fartingPower: 3, name: "Martin"},
 	}
 
-	board[0][0] = "X"
-	board[2][2] = "O"
-	board[1][2] = "X"
-	board[1][0] = "O"
-	board[0][2] = "X"
-
-	for i := 0; i < len(board); i++ {
-		fmt.Printf("%s\n", strings.Join(board[i], " "))
-	}
+	fmt.Println("yo", players)
 }
